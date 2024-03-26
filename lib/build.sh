c89f258b8860701ba704ee58bae4589f4ef1db2b
fridell <fritz.ritzberger@chello.at>
2024-03-26 22:21:08 +0100

    Removed wrong build.sh in lib dir

--- lib/build.sh
+++
@@ -1,8 +0,0 @@
-# Display Java version
-java -version
-
-# Refer your ANT tool in following CLASSPATH
-CLASSPATH=.:../ant/ant.jar:../ant/xml-apis.jar:../ant/xercesImpl.jar
-
-java -cp "$CLASSPATH" org.apache.tools.ant.Main $*
-# there may be simple command lines ...
